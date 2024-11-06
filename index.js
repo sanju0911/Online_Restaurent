0b03209576ebcaab6f139349be45fda7f5e6f54f
sanju <sanjayjanardhan231@gmail.com>
2024-11-06 17:24:03 +0530

    added dotenv

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const mongoose = require("mongoose");
 const dotenv = require("dotenv").config();
 const OwnerRoutes = require("./routes/OwnerRoutes");
@@ -8,7 +8,6 @@ const RestuarentRoutes = require("./routes/Restuarentroutes");
 const ProductRoutes = require("./routes/ProductsRoutes");
 const bodyParser = require("body-parser");
 const path = require("path");
-
 
 app.use(bodyParser.json());
 
@@ -24,7 +23,7 @@ mongoose
 app.use("/Owner", OwnerRoutes);
 app.use("/Restuarent", RestuarentRoutes);
 app.use("/Products", ProductRoutes);
-app.use("/uploads" , express.static('uploads'));
+app.use("/uploads", express.static("uploads"));
 
 app.listen(port, () => {
   console.log("server is running on port 3000");
